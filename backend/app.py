f70bba15d16375781f07333d4c1c368df8382ab1
Bravin <onsasebravin853@gmail.com>
2024-01-28 21:33:12 +0300

    app config in app.py

--- backend/app.py
+++ backend/app.py
@@ -2,3 +2,8 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from models import db, Hero, Power, HeroPower
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///supes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
