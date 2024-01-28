1b31096bc16574497e1f49cd592f249ac40e2687
Bravin <onsasebravin853@gmail.com>
2024-01-28 21:34:38 +0300

    migrate in app.py

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///supes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
