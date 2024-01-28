ebedd7b07ecde57532ef0683e6c3c9251fb80cd9
Bravin <onsasebravin853@gmail.com>
2024-01-28 21:48:53 +0300

    hero class in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Hero(db.Model):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(100), nullable=False)
+
+    # Define the relationship between Hero and HeroPower
+    powers = db.relationship('HeroPower', back_populates='hero')
