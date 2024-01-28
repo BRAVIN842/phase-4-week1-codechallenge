89936efe58a1fb8c4b202559d7f2e6b62e722560
Bravin <onsasebravin853@gmail.com>
2024-01-28 21:36:19 +0300

    get method in heroes

--- backend/app.py
+++ backend/app.py
@@ -11,3 +11,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+# Sample API route to get all heroes
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    print("Received GET request at /heroes")
+    heroes = Hero.query.all()
+    heroes_list = [{
+        'id': hero.id,
+        'name': hero.name,
+        'super_name': hero.super_name
+    } for hero in heroes]
+
+    response = make_response(jsonify(heroes_list), 200)
+    return response
